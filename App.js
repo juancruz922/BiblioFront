ff75485359c19c91ff47ac4e658c4ff81a50aee7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-04 10:40:51 -0300

    intento de cambiar pantallas

--- App.js
+++ App.js
@@ -3,9 +3,21 @@ import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 import Main from './src/components/Main'
 import Segunda from './src/components/2'
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
  
-  return (<Main/>);
+  return (
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Segunda" component={Segunda} />
+      </Stack.Navigator>
+  </NavigationContainer>
+  )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image, Button } from 'react-native';
+import Segunda from './2.jsx'
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>BibliotecaOrt</Text>
+      <View style={styles.content}>
+       
+        <View style={styles.rectangle}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="  D.N.I" />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="  Contraseña" secureTextEntry={true} />
+        </View>
+        <TouchableOpacity style={[styles.button, styles.loginButton]}>
+          <Button
+           style={styles.buttonText}
+            title="Iniciar sesión"
+            onPress={() => navigation.navigate(Segunda)}
+               ></Button>
+        </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#0D47A1',
+    marginBottom: 40,
+    fontFamily: 'serif',
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    marginTop: '8%',
+    alignItems: 'center',
+   
+  },
+  inputContainer: {
+    marginBottom: 20,
+    width: '80%',
+  },
+  input: {
+    height: 55,
+    borderWidth: 2,
+    width: '100%',
+    borderRadius: 20,
+    marginLeft: 27
+  },
+  button: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#0D47A1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 85,
+    marginBottom: 10,
+    borderRadius: 20,
+    marginLeft: 25,
+  },
+  buttonText: {
+    fontSize: 25,
+    color: '#FFFFFF',
+  },
+  loginButton: {
+    backgroundColor: '#0D47A1',
+  },
+  rectangle: {
+    backgroundColor: '#E0E0E0',
+    width: '80%',
+    borderRadius: 20,
+    height: '150%',
+    marginTop: 0,
+    display: "flex",
+    justifyContent: 'center',
+  
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: -10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default App;
