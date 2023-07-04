b844a7e846b4b120181d0d6c718dd358dc560cc6
juancruz922 <juancruzgabrioloo@gmail.com>
2023-07-04 09:50:25 -0300

    inicio Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import Main from './src/components/Main'
+import Segunda from './src/components/2'
+import React from 'react';
+
+export default function App() {
+ 
+  return (<Main/>);
+}
+
