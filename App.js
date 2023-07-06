e7f7167f4e51c3226ae29ef76f1c475f7bf5605e
juancruz922 <juancruzgabrioloo@gmail.com>
2023-07-06 19:15:46 -0300

    menu hamburguesa

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 import Main from './src/components/Main'
-import Segunda from './src/components/2'
-import Tercera from './src/components/3'
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Segunda from './src/components/Segunda'
+import Tercera from './src/components/Tercera'
 
-const Stack = createNativeStackNavigator();
 
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { Contacto } from "./pages/Contacto";
+import { Acercade } from "./pages/Acercade";
+import { Inicio } from "./pages/Inicio";
+
+const Menu = createDrawerNavigator();
 
 export default function App() {
- 
-  return (<Main/>)
-    
-  /*<NavigationContainer>
-<Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Segunda" component={Segunda} />
-      </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
-*/
+  return (
+    <NavigationContainer>
+      <Menu.Navigator>
+        <Menu.Screen
+          name="Inicio"
+          options={{
+            headerTitle: "Nuevo titulo",
+          }}
+          component={Inicio}
+        />
+        <Menu.Screen name="Segunda" component={Segunda} />
+        <Menu.Screen name="Tercera" component={Tercera} />
+      </Menu.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ src/components/Segunda.jsx
@@ -0,0 +1,158 @@
+import { Text,StyleSheet, View } from 'react-native';
+
+export function Segunda() {
+  return (
+    <View style={styles.container}>
+      <Text>2</Text>
+    </View>
+  );
+}
+
+
+  
+
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image } from 'react-native';
+
+const SecondScreen = () => {
+  const handleRayasPress = () => {
+    console.log('Botón de rayas presionado');
+  };
+
+  return (
+    <View style={styles.containerb}>
+      <View style={styles.content}>
+        <View style={styles.blueRectangle}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={handleRayasPress}>
+              <Image source={require('../../assets/rayas.png')} style={styles.rayas} />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.blueRectangleTitle}>RETIRO</Text>
+        </View>
+        <View style={styles.grayRectangle}>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.input} placeholder="Seleccione un libro" />
+            <TextInput style={styles.input} placeholder="Agregue una nota sobre su libro" />
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Generar QR</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      </View>
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerb: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+
+    width: '100%',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginBottom: -50,
+    width: '100%',
+  },
+  menuIconContainer: {
+    marginRight: 10,
+  },
+  menuIcon: {
+    width: 20,
+    height: 20,
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+  },
+  blueRectangle: {
+    width: '100%',
+    height: '20%',
+    backgroundColor: '#0D47A1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  blueRectangleTitle: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'serif',
+  },
+  grayRectangle: {
+    width: '80%',
+    height: '70%',
+    backgroundColor: '#E0E0E0',
+    borderRadius: 10,
+    marginTop: 20,
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    marginTop: 70,
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#0D47A1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+  },
+  footer: {
+    alignItems: 'center',
+    marginTop: 'auto',
+    marginBottom: 20,
+    width: '100%',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+
+  },
+  rayas:{
+
+    width: 35,
+    height: 35,
+    marginBottom: 20,
+    marginRight: 80,
+  }
+});
+
+export default SecondScreen;
