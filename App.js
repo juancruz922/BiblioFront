28a025a8fa6231c3906b30e19353d5d7e843326a
juancruz922 <juancruzgabrioloo@gmail.com>
2023-07-06 20:02:46 -0300

    si anda gano

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ import Tercera from './src/components/Tercera'
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
+import { Contacto } from "./pages/Contacto";
+import { Acercade } from "./pages/Acercade";
+import { Inicio } from "./pages/Inicio";
+
 const Menu = createDrawerNavigator();
 
 export default function App() {
@@ -23,6 +27,13 @@ export default function App() {
         />
         <Menu.Screen name="Retirar" component={Segunda} />
         <Menu.Screen name="Mis Prestamos" component={Tercera} />
+          name="Inicio"
+          options={{
+            headerTitle: "Nuevo titulo",
+          }}
+          component={Inicio}
+        <Menu.Screen name="Segunda" component={Segunda} />
+        <Menu.Screen name="Tercera" component={Tercera} />
       </Menu.Navigator>
     </NavigationContainer>
   );
