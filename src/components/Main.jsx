ca1af66605329e641b82624f12e5e8ef529e55aa
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-08 12:06:11 -0300

    inicio sesion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,34 +2,82 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Segunda from './Segunda.jsx'
+import { Tercera } from './Tercera.jsx';
+import { interpolate } from 'react-native-reanimated';
 
 
-const App = ({navigation}) => {
-  const irASegundaPantalla = () => {
-  navigation.navigate('Segunda');
-};
 
+const App = ({ navigation }) => {
+  const [dni, setDNI] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
+  // Definir el array de alumnos con DNI y contraseña
+  const alumnos = [
+    { dni: '11111111A', contraseña: 'clave1' },
+    { dni: '22222222B', contraseña: 'clave2' },
+    { dni: '33333333C', contraseña: 'clave3' },
+    { dni: '44444444D', contraseña: 'clave4' },
+    { dni: '55555555E', contraseña: 'clave5' },
+  ];
+
+  // Definir el array de bibliotecarios con DNI y contraseña
+  const bibliotecarios = [
+    { dni: '66666666F', contraseña: 'clave6' },
+    { dni: '77777777G', contraseña: 'clave7' },
+    { dni: '88888888H', contraseña: 'clave8' },
+    { dni: '99999999I', contraseña: 'clave9' },
+    { dni: '10101010J', contraseña: 'clave10' },
+  ];
+
+  const irAPantalla = () => {
+    // Verificar si el DNI y la contraseña coinciden con un alumno
+    const alumnoEncontrado = alumnos.find(
+      (alumno) => alumno.dni === dni && alumno.contraseña === contraseña
+    );
+
+    // Verificar si el DNI y la contraseña coinciden con un bibliotecario
+    const bibliotecarioEncontrado = bibliotecarios.find(
+      (bibliotecario) => bibliotecario.dni === dni && bibliotecario.contraseña === contraseña
+    );
+
+    if (alumnoEncontrado) {
+      // Navegar a la pantalla de alumno
+      navigation.navigate(Segunda);
+    } else if (bibliotecarioEncontrado) {
+      // Navegar a la pantalla de bibliotecario
+      navigation.navigate(Tercera);
+    } else {
+      // Mostrar un mensaje de error
+    }
+  };
   
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Biblioteca Ort</Text>
       <View style={styles.content}>
-
         <View style={styles.rectangle}>
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder="  D.N.I" />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder="  Contraseña" secureTextEntry={true} />
-        </View>
-        <TouchableOpacity style={[styles.button]}>
-          <Button 
-          
-            title="Iniciar sesión" 
-            onPress= {irASegundaPantalla}
-            style={styles.login}
-          ></Button>
-        </TouchableOpacity>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="  D.N.I"
+              onChangeText={(text) => setDNI(text)}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="  Contraseña"
+              secureTextEntry={true}
+              onChangeText={(text) => setContraseña(text)}
+            />
+          </View>
+          <TouchableOpacity style={[styles.button]}>
+            <Button
+              title="Iniciar sesión"
+              onPress={irAPantalla}
+              style={styles.login}
+            ></Button>
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.imageContainer}>
@@ -37,7 +85,6 @@ const App = ({navigation}) => {
       </View>
     </View>
   );
-
 };
 
 const styles = StyleSheet.create({
