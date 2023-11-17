90a3f15f16606a1713fcb737b21756d91a402ad7
Nicolaspereira2005 <pereiranicolas507@gmail.com>
2023-11-17 01:04:14 -0300

    ..

---
+++ src/data/prestamos.js
@@ -0,0 +1,29 @@
+const prestamos = [
+    {
+      idPrestamo: 1,
+      idLibro: 1,
+      idAlumno: 1,
+      nota: 'Nota del Prestamo 1',
+      fechaInicio: new Date('2023-10-27'),
+      fechaFinal: new Date('2023-11-10'),
+    },
+    {
+      idPrestamo: 2,
+      idLibro: 2,
+      idAlumno: 2,
+      nota: 'Nota del Prestamo 2',
+      fechaInicio: new Date('2023-10-28'),
+      fechaFinal: new Date('2023-11-11'),
+    },
+    {
+      idPrestamo: 3,
+      idLibro: 3,
+      idAlumno: 3,
+      nota: 'Nota del Prestamo 3',
+      fechaInicio: new Date('2023-10-29'),
+      fechaFinal: new Date('2023-11-12'),
+    },
+  ];
+  
+  export default prestamos;
+  
--- src/data/usuarios.js
+++ src/data/usuarios.js
@@ -1,25 +1,39 @@
 const usuarios = [
-
     {
-        idUsuario: 1,
-        nombreUsuario: "1",
-        nombre: "Binker",
-        contraseña: "1",
-        rol: 1 //admin
+      idUsuario: 1,
+      nombreUsuario: "admin1",
+      nombre: "Binker",
+      contraseña: "adminpass1",
+      rol: 1, // admin
     },
     {
-        idUsuario: 3,
-        nombreUsuario: "3",
-        nombre: "Kristal",
-        contraseña: "3",
-        rol: 1 //admin
+      idUsuario: 3,
+      nombreUsuario: "admin2",
+      nombre: "Kristal",
+      contraseña: "adminpass2",
+      rol: 1, // admin
     },
     {
-        idUsuario: 2,
-        nombreUsuario: "2",
-        contraseña: "2",
-        rol: 2 //usuario
-    }
-    
-]
-export default usuarios;
+      idUsuario: 2,
+      nombreUsuario: "usuario1",
+      contraseña: "userpass1",
+      rol: 2, // usuario
+    },
+    {
+      idUsuario: 4,
+      nombreUsuario: "usuario2",
+      nombre: "John",
+      contraseña: "userpass2",
+      rol: 2, // usuario
+    },
+    {
+      idUsuario: 5,
+      nombreUsuario: "usuario3",
+      nombre: "Alice",
+      contraseña: "userpass3",
+      rol: 2, // usuario
+    },
+  ];
+  
+  export default usuarios;
+  
